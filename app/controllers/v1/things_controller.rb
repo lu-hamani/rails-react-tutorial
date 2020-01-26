71131c172e091bd3958db55e16c175600df17f1f
lucas hamani <hamanilucas@gmail.com>
2020-01-26 17:45:38 +0100

    step 1

---
+++ app/controllers/v1/things_controller.rb
@@ -0,0 +1,10 @@
+class V1::ThingsController < ApplicationController
+  def index
+    render json: { :things => [
+      {
+        :name => 'some-thing',
+        :guid => 'zzkzkfzkfzkzfkfkfkz'
+      }
+    ] }.to_json
+  end
+end
